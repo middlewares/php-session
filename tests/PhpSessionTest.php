5224f8f89954b18b9cbd47f913e539c3570729f7
oscarotero <oom@oscarotero.com>
2018-05-12 11:05:56 +0200

    tested id regeneration

--- tests/PhpSessionTest.php
+++ tests/PhpSessionTest.php
@@ -48,4 +48,31 @@ class PhpSessionTest extends TestCase
         $this->assertEquals($sessionName, (string) $response->getBody());
         $this->assertEquals($value, $_SESSION['name']);
     }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testRegenerateId()
+    {
+        $response = Dispatcher::run(
+            [
+                (new PhpSession())
+                    ->id('1234')
+                    ->regenerateId(-10)
+                    ->options([
+                        'use_cookies' => false,
+                        'use_only_cookies' => true,
+                    ]),
+
+                function () {
+                    $this->assertNotEquals('1234', session_id());
+                    $this->assertTrue(isset($_SESSION['session-id-expires']));
+
+                    echo 'ok';
+                },
+            ]
+        );
+
+        $this->assertEquals('ok', (string) $response->getBody());
+    }
 }
