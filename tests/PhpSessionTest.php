b02f733d62ab144505254c9ccb32796a3c2f308c
Jonathon Hill <jhill9693@gmail.com>
2021-02-20 12:07:22 -0500

    Output session cookie when the session ID changes
    Resolves https://github.com/middlewares/php-session/issues/4

--- tests/PhpSessionTest.php
+++ tests/PhpSessionTest.php
@@ -6,46 +6,153 @@ namespace Middlewares\Tests;
 use Middlewares\PhpSession;
 use Middlewares\Utils\Dispatcher;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class PhpSessionTest extends TestCase
 {
+    private $sessionOptions = [
+        'use_strict_mode' => false,
+        'use_trans_sid' => false,
+        'use_cookies' => false,
+        'use_only_cookies' => true,
+        'cache_limiter' => '',
+        'cookie_lifetime' => 60 * 60 * 24 * 30, // 30 days
+        'cookie_path' => '/path',
+        'cookie_domain' => 'domain.tld',
+        'cookie_secure' => true,
+        'cookie_httponly' => true,
+        'cookie_samesite' => 'Strict',
+    ];
+
+    private function getCookieHeader(string $sessionName, string $sessionId): string
+    {
+        return sprintf(
+            '%s=%s; expires=%s; path=%s; domain=%s; secure; httponly; SameSite=%s',
+            urlencode($sessionName),
+            urlencode($sessionId),
+            $this->sessionOptions['cookie_path'],
+            $this->sessionOptions['cookie_domain'],
+            gmdate('D, d M Y H:i:s T', $this->sessionOptions['cookie_lifetime']),
+            $this->sessionOptions['cookie_samesite'],
+        );
+    }
+
     public function sessionDataProvider(): array
     {
         return [
             [
                 'session_1',
+                session_create_id(),
                 'Iván',
             ], [
                 'session_2',
+                session_create_id(),
                 'Pablo',
             ],
         ];
+    }
+
+    public function testCheckUseTransSidSettingException()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('session.use_trans_sid must be false');
+
+        (new PhpSession())->options([
+            'use_trans_sid' => true,
+        ]);
+    }
+
+    public function testCheckUseCookiesSettingException()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('session.use_cookies must be false');
+
+        (new PhpSession())->options([
+            'use_trans_sid' => false,
+            'use_cookies' => true,
+        ]);
+    }
+
+    public function testCheckUseOnlyCookiesSettingException()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('session.use_only_cookies must be true');
+
+        (new PhpSession())->options([
+            'use_trans_sid' => false,
+            'use_cookies' => false,
+            'use_only_cookies' => false,
+        ]);
+    }
+
+    public function testCheckCacheLimiterSettingException()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('session.cache_limiter must be set to an empty string');
+
+        (new PhpSession())->options([
+            'use_trans_sid' => false,
+            'use_cookies' => false,
+            'use_only_cookies' => true,
+            'cache_limiter' => 'nocache',
+        ]);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testDefaultSettingCheck()
+    {
+        ini_set('session.use_trans_sid', '0');
+        ini_set('session.use_cookies', '0');
+        ini_set('session.use_only_cookies', '1');
+        ini_set('session.cache_limiter', '');
+        $instance = new PhpSession();
+        $this->assertEquals($instance->options([]), $instance);
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testWriteSessionCookie()
+    {
+        $response = Dispatcher::run(
+            [
+                (new PhpSession())->options($this->sessionOptions),
+
+                function () {
+                    echo 'ok';
+                },
+            ]
+        );
+
+        $this->assertTrue($response->hasHeader('Set-Cookie'));
+        $this->assertEquals('ok', (string) $response->getBody());
     }
 
     /**
      * @runInSeparateProcess
      * @dataProvider sessionDataProvider
      */
-    public function testPhpSession(string $sessionName, string $value)
+    public function testPhpSession(string $sessionName, string $sessionId, string $value)
     {
         $response = Dispatcher::run(
             [
                 (new PhpSession())
                     ->name($sessionName)
-                    ->options([
-                        'use_cookies' => false,
-                        'use_only_cookies' => true,
-                    ]),
+                    ->id($sessionId)
+                    ->options($this->sessionOptions),
 
                 function ($request) use ($value) {
-                    echo session_name();
+                    echo session_name() . ':' . session_id();
 
                     $_SESSION['name'] = $value;
                 },
             ]
         );
 
-        $this->assertEquals($sessionName, (string) $response->getBody());
+        $this->assertFalse($response->hasHeader('Set-Cookie'));
+        $this->assertEquals($sessionName . ':' . $sessionId, (string) $response->getBody());
         $this->assertEquals($value, $_SESSION['name']);
     }
 
@@ -54,18 +161,17 @@ class PhpSessionTest extends TestCase
      */
     public function testRegenerateId()
     {
+        $sessionId = session_create_id();
+
         $response = Dispatcher::run(
             [
                 (new PhpSession())
-                    ->id('1234')
+                    ->id($sessionId)
                     ->regenerateId(-10)
-                    ->options([
-                        'use_cookies' => false,
-                        'use_only_cookies' => true,
-                    ]),
+                    ->options($this->sessionOptions),
 
-                function () {
-                    $this->assertNotEquals('1234', session_id());
+                function () use ($sessionId) {
+                    $this->assertNotEquals($sessionId, session_id());
                     $this->assertTrue(isset($_SESSION['session-id-expires']));
 
                     echo 'ok';
@@ -73,6 +179,32 @@ class PhpSessionTest extends TestCase
             ]
         );
 
+        $this->assertTrue($response->hasHeader('Set-Cookie'));
         $this->assertEquals('ok', (string) $response->getBody());
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testStrictMode()
+    {
+        $sessionId = session_create_id();
+
+        $response = Dispatcher::run(
+            [
+                (new PhpSession())
+                    ->id($sessionId)
+                    ->options(array_merge($this->sessionOptions, [
+                        'use_strict_mode' => true,
+                    ])),
+
+                function () use ($sessionId) {
+                    echo session_id();
+                },
+            ]
+        );
+
+        $this->assertTrue($response->hasHeader('Set-Cookie'));
+        $this->assertNotEquals($sessionId, (string) $response->getBody());
     }
 }
